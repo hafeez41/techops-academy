981db8b40dab3e42884a97bde3c63cfcbebf35bc
Abdulhafiz abubakar <hafeez41@users.noreply.github.com>
2026-04-05 19:39:45 +0100

    Add img.youtube.com to next/image remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,7 @@ const nextConfig = {
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "img.youtube.com" },
     ],
   },
 };
